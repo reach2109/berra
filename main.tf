a5d2b35bccec076d3e3c7a00df51701bbc99ca65
reach2109 <reachable_now@outlook.com>
2022-07-31 20:36:21 +1000

    Changed the ami type

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "example" {
-  ami = "ami-0cff7528ff583bf9a"
+  ami = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
   
   tags = {
